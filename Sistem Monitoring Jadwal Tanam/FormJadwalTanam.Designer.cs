02bdfc6ededed721a0943646b56637b36b838b55
Bagaswidhi <Bagaswidhi10@gmail.com>
2026-04-16 03:09:12 +0700

    Menambahkan string idJadwalTerpilih sebagai parameter dan fungsi berishkanForm

--- Sistem Monitoring Jadwal Tanam/FormJadwalTanam.Designer.cs
+++ Sistem Monitoring Jadwal Tanam/FormJadwalTanam.Designer.cs
@@ -36,47 +36,56 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_Search = new System.Windows.Forms.TextBox();
+            this.txtIdTanaman = new System.Windows.Forms.TextBox();
+            this.txtIdLahan = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEstimasiPanen = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpTanggalTanam = new System.Windows.Forms.DateTimePicker();
+            this.btn_Back = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_Insert
             // 
-            this.btn_Insert.Location = new System.Drawing.Point(225, 356);
+            this.btn_Insert.Location = new System.Drawing.Point(232, 374);
             this.btn_Insert.Name = "btn_Insert";
             this.btn_Insert.Size = new System.Drawing.Size(132, 42);
             this.btn_Insert.TabIndex = 0;
             this.btn_Insert.Text = "Menambah Data";
             this.btn_Insert.UseVisualStyleBackColor = true;
+            this.btn_Insert.Click += new System.EventHandler(this.btn_Insert_Click);
             // 
             // btn_Load
             // 
-            this.btn_Load.Location = new System.Drawing.Point(15, 356);
+            this.btn_Load.Location = new System.Drawing.Point(16, 374);
             this.btn_Load.Name = "btn_Load";
             this.btn_Load.Size = new System.Drawing.Size(132, 42);
             this.btn_Load.TabIndex = 1;
             this.btn_Load.Text = "Menampilkan Data";
             this.btn_Load.UseVisualStyleBackColor = true;
+            this.btn_Load.Click += new System.EventHandler(this.btn_Load_Click);
             // 
             // btn_Update
             // 
-            this.btn_Update.Location = new System.Drawing.Point(435, 356);
+            this.btn_Update.Location = new System.Drawing.Point(449, 374);
             this.btn_Update.Name = "btn_Update";
             this.btn_Update.Size = new System.Drawing.Size(135, 42);
             this.btn_Update.TabIndex = 2;
             this.btn_Update.Text = "Mengubah Data";
             this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
             // 
             // btn_Delete
             // 
-            this.btn_Delete.Location = new System.Drawing.Point(644, 356);
+            this.btn_Delete.Location = new System.Drawing.Point(666, 374);
             this.btn_Delete.Name = "btn_Delete";
             this.btn_Delete.Size = new System.Drawing.Size(122, 42);
             this.btn_Delete.TabIndex = 3;
             this.btn_Delete.Text = "Menghapus Data";
             this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
             // 
             // dataGridView1
             // 
@@ -85,8 +94,9 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(764, 252);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 252);
             this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // label1
             // 
@@ -100,52 +110,100 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(504, 308);
+            this.label2.Location = new System.Drawing.Point(154, 308);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(60, 16);
             this.label2.TabIndex = 6;
-            this.label2.Text = "label2";
+            this.label2.Text = "ID Lahan";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(97, 308);
+            this.label3.Location = new System.Drawing.Point(34, 308);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.Size = new System.Drawing.Size(81, 16);
             this.label3.TabIndex = 7;
-            this.label3.Text = "label3";
+            this.label3.Text = "ID Tanaman";
             // 
-            // comboBox1
+            // txt_Search
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(147, 305);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 8;
+            this.txt_Search.Location = new System.Drawing.Point(77, 6);
+            this.txt_Search.Name = "txt_Search";
+            this.txt_Search.Size = new System.Drawing.Size(594, 22);
+            this.txt_Search.TabIndex = 10;
+            this.txt_Search.TextChanged += new System.EventHandler(this.textSearch_TextChanged);
             // 
-            // comboBox2
+            // txtIdTanaman
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(554, 305);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
-            this.comboBox2.TabIndex = 9;
+            this.txtIdTanaman.Location = new System.Drawing.Point(46, 327);
+            this.txtIdTanaman.Name = "txtIdTanaman";
+            this.txtIdTanaman.Size = new System.Drawing.Size(60, 22);
+            this.txtIdTanaman.TabIndex = 11;
             // 
-            // textBox1
+            // txtIdLahan
             // 
-            this.textBox1.Location = new System.Drawing.Point(77, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(699, 22);
-            this.textBox1.TabIndex = 10;
+            this.txtIdLahan.Location = new System.Drawing.Point(157, 327);
+            this.txtIdLahan.Name = "txtIdLahan";
+            this.txtIdLahan.Size = new System.Drawing.Size(55, 22);
+            this.txtIdLahan.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(314, 308);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 16);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Tanggal Tanam";
+            // 
+            // txtEstimasiPanen
+            // 
+            this.txtEstimasiPanen.Location = new System.Drawing.Point(513, 329);
+            this.txtEstimasiPanen.Name = "txtEstimasiPanen";
+            this.txtEstimasiPanen.ReadOnly = true;
+            this.txtEstimasiPanen.Size = new System.Drawing.Size(100, 22);
+            this.txtEstimasiPanen.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(513, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Estimasi Panen";
+            // 
+            // dtpTanggalTanam
+            // 
+            this.dtpTanggalTanam.Location = new System.Drawing.Point(267, 327);
+            this.dtpTanggalTanam.Name = "dtpTanggalTanam";
+            this.dtpTanggalTanam.Size = new System.Drawing.Size(200, 22);
+            this.dtpTanggalTanam.TabIndex = 17;
+            // 
+            // btn_Back
+            // 
+            this.btn_Back.BackColor = System.Drawing.Color.Red;
+            this.btn_Back.Location = new System.Drawing.Point(713, 5);
+            this.btn_Back.Name = "btn_Back";
+            this.btn_Back.Size = new System.Drawing.Size(75, 23);
+            this.btn_Back.TabIndex = 18;
+            this.btn_Back.Text = "Back";
+            this.btn_Back.UseVisualStyleBackColor = false;
+            this.btn_Back.Click += new System.EventHandler(this.btn_Back_Click);
             // 
             // FormJadwalTanam
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_Back);
+            this.Controls.Add(this.dtpTanggalTanam);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEstimasiPanen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtIdLahan);
+            this.Controls.Add(this.txtIdTanaman);
+            this.Controls.Add(this.txt_Search);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -156,6 +214,7 @@
             this.Controls.Add(this.btn_Insert);
             this.Name = "FormJadwalTanam";
             this.Text = "FormJadwalTanam";
+            this.Load += new System.EventHandler(this.FormJadwalTanam_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -172,8 +231,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_Search;
+        private System.Windows.Forms.TextBox txtIdTanaman;
+        private System.Windows.Forms.TextBox txtIdLahan;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEstimasiPanen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpTanggalTanam;
+        private System.Windows.Forms.Button btn_Back;
     }
 }
--- Sistem Monitoring Jadwal Tanam/FormJadwalTanam.cs
+++ Sistem Monitoring Jadwal Tanam/FormJadwalTanam.cs
@@ -11,15 +11,29 @@ using System.Data.SqlClient;
 
 namespace Sistem_Monitoring_Jadwal_Tanam
 {
-    public partial class FormJadwalTanam: Form
+    public partial class FormJadwalTanam : Form
     {
         private readonly SqlConnection conn;
         private readonly string connectionString = "Data Source=MSI\\BAGASWIDHI;Initial Catalog=DBSistemMonitoringMasaTanam;Integrated Security=True";
+        private string idJadwalTerpilih = "";
         public FormJadwalTanam()
         {
             InitializeComponent();
             conn = new SqlConnection(connectionString);
 
+        }
+
+        private void BersihkanForm()
+        {
+            txtIdTanaman.Clear();
+            txtIdLahan.Clear();
+            txtEstimasiPanen.Clear(); // TextBox ini diset ReadOnly=true
+
+            // Kembalikan kalender ke hari ini
+            dtpTanggalTanam.Value = DateTime.Now;
+
+            idJadwalTerpilih = "";
+            txtIdTanaman.Focus();
         }
     }
 }
