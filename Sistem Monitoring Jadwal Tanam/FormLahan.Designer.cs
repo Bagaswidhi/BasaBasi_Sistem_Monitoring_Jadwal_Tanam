2f02ebd6fd6a17b9d6b4ac56c7eb7bb4fb64c601
Bagaswidhi <Bagaswidhi10@gmail.com>
2026-04-15 23:30:57 +0700

    Menambah fitur insert pada Form Lahan beserta fungsinya

--- Sistem Monitoring Jadwal Tanam/FormLahan.Designer.cs
+++ Sistem Monitoring Jadwal Tanam/FormLahan.Designer.cs
@@ -34,6 +34,14 @@
             this.btn_Delete = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btn_Back = new System.Windows.Forms.Button();
+            this.txtNamaLahan = new System.Windows.Forms.TextBox();
+            this.txtLuasLahan = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLahanID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Search = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,6 +73,7 @@
             this.btn_Update.TabIndex = 2;
             this.btn_Update.Text = "Mengubah Data";
             this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
             // 
             // btn_Delete
             // 
@@ -74,31 +83,108 @@
             this.btn_Delete.TabIndex = 3;
             this.btn_Delete.Text = "Menghapus Data";
             this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 57);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(517, 351);
+            this.dataGridView1.Size = new System.Drawing.Size(517, 306);
             this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btn_Back
             // 
-            this.btn_Back.Location = new System.Drawing.Point(713, 12);
+            this.btn_Back.BackColor = System.Drawing.Color.Red;
+            this.btn_Back.Location = new System.Drawing.Point(706, 12);
             this.btn_Back.Name = "btn_Back";
-            this.btn_Back.Size = new System.Drawing.Size(75, 23);
+            this.btn_Back.Size = new System.Drawing.Size(82, 29);
             this.btn_Back.TabIndex = 5;
             this.btn_Back.Text = "Back";
-            this.btn_Back.UseVisualStyleBackColor = true;
+            this.btn_Back.UseVisualStyleBackColor = false;
+            this.btn_Back.Click += new System.EventHandler(this.btn_Back_Click);
+            // 
+            // txtNamaLahan
+            // 
+            this.txtNamaLahan.Location = new System.Drawing.Point(607, 154);
+            this.txtNamaLahan.Name = "txtNamaLahan";
+            this.txtNamaLahan.Size = new System.Drawing.Size(100, 22);
+            this.txtNamaLahan.TabIndex = 6;
+            // 
+            // txtLuasLahan
+            // 
+            this.txtLuasLahan.Location = new System.Drawing.Point(607, 211);
+            this.txtLuasLahan.Name = "txtLuasLahan";
+            this.txtLuasLahan.Size = new System.Drawing.Size(100, 22);
+            this.txtLuasLahan.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(604, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "LahanID";
+            // 
+            // txtLahanID
+            // 
+            this.txtLahanID.Location = new System.Drawing.Point(607, 95);
+            this.txtLahanID.Name = "txtLahanID";
+            this.txtLahanID.Size = new System.Drawing.Size(100, 22);
+            this.txtLahanID.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(604, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nama Lahan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(604, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Luas Lahan";
+            // 
+            // txt_Search
+            // 
+            this.txt_Search.AutoSize = true;
+            this.txt_Search.Location = new System.Drawing.Point(8, 22);
+            this.txt_Search.Name = "txt_Search";
+            this.txt_Search.Size = new System.Drawing.Size(50, 16);
+            this.txt_Search.TabIndex = 12;
+            this.txt_Search.Text = "Search";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(465, 22);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // FormLahan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_Search);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLahanID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLuasLahan);
+            this.Controls.Add(this.txtNamaLahan);
             this.Controls.Add(this.btn_Back);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btn_Delete);
@@ -107,8 +193,10 @@
             this.Controls.Add(this.btn_Insert);
             this.Name = "FormLahan";
             this.Text = "FormLahan";
+            this.Load += new System.EventHandler(this.FormLahan_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -120,5 +208,13 @@
         private System.Windows.Forms.Button btn_Delete;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btn_Back;
+        private System.Windows.Forms.TextBox txtNamaLahan;
+        private System.Windows.Forms.TextBox txtLuasLahan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLahanID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txt_Search;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Sistem Monitoring Jadwal Tanam/FormLahan.cs
+++ Sistem Monitoring Jadwal Tanam/FormLahan.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 
 namespace Sistem_Monitoring_Jadwal_Tanam
 {
-    public partial class FormLahan : Form
+    public partial class FormLahan: Form
     {
         private readonly SqlConnection conn;
         private readonly string connectionString = "Data Source=MSI\\BAGASWIDHI;Initial Catalog=DBSistemMonitoringMasaTanam;Integrated Security=True";
@@ -20,4 +20,51 @@ namespace Sistem_Monitoring_Jadwal_Tanam
             InitializeComponent();
             conn = new SqlConnection(connectionString);
         }
+        private void btn_Insert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
 
+                if (txtNamaLahan.Text == "")
+                {
+                    MessageBox.Show("Nama Lahan tidak boleh kosong.");
+                    txtNamaLahan.Focus();
+                    return;
+                }
+
+                if (txtLuasLahan.Text == "")
+                {
+                    MessageBox.Show("Luas Lahan tidak boleh kosong.");
+                    txtLuasLahan.Focus();
+                    return;
+                }
+                string query = @"INSERT INTO DataLahan (NamaLahan, luas_Lahan) VALUES (@NamaLahan, @LuasLahan)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@NamaLahan", txtNamaLahan.Text);
+                cmd.Parameters.AddWithValue("@LuasLahan", txtLuasLahan.Text);
+                int result = cmd.ExecuteNonQuery();
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Data berhasil ditambahkan.");
+                    txtNamaLahan.Clear();
+                    txtLuasLahan.Clear();
+                    btn_Load.PerformClick();
+                }
+                else
+                {
+                    MessageBox.Show("Gabisa nambahin data.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
