603e2ae0533615592a641f8a92937eb817fd2329
Bagaswidhi <Bagaswidhi10@gmail.com>
2026-04-15 20:22:57 +0700

    Menambahkan Fungsi button load pada FormTanaman

--- Sistem Monitoring Jadwal Tanam/FormTanaman.cs
+++ Sistem Monitoring Jadwal Tanam/FormTanaman.cs
@@ -20,5 +20,42 @@ namespace Sistem_Monitoring_Jadwal_Tanam
             InitializeComponent();
             conn = new SqlConnection(connectionString);
         }
+
+        private void btn_Load_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                dataGridView1.Columns.Add("TanamanID", "Tanaman ID");
+                dataGridView1.Columns.Add("NamaTanaman", "Nama Tanaman");
+                dataGridView1.Columns.Add("LamaMasaTanam", "Lama Masa Tanam (hari)");
+
+                string query = "SELECT * FROM DataTanaman";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["TanamanID"].ToString(),
+                        reader["NamaTanaman"].ToString(),
+                        reader["LamaMasaTanam"].ToString()
+                        );
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
 }
