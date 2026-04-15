420a2b0fd850e0a32f562cb2c61c582d550f023a
Bagaswidhi <Bagaswidhi10@gmail.com>
2026-04-16 03:11:24 +0700

    Menambahkan Fungsi Load formJadwalTanam dan grid view untuk tampilan data

--- Sistem Monitoring Jadwal Tanam/FormJadwalTanam.cs
+++ Sistem Monitoring Jadwal Tanam/FormJadwalTanam.cs
@@ -35,5 +35,38 @@ namespace Sistem_Monitoring_Jadwal_Tanam
             idJadwalTerpilih = "";
             txtIdTanaman.Focus();
         }
+
+        private void FormJadwalTanam_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.CellClick += dataGridView1_CellContentClick;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                idJadwalTerpilih = row.Cells["JadwalID"].Value.ToString();
+                txtIdTanaman.Text = row.Cells["TanamanID"].Value.ToString();
+                txtIdLahan.Text = row.Cells["LahanID"].Value.ToString();
+                dtpTanggalTanam.Value = Convert.ToDateTime(row.Cells["TanggalTanam"].Value);
+                txtEstimasiPanen.Text = Convert.ToDateTime(row.Cells["EstimasiPanen"].Value).ToString("yyyy-MM-dd");
+
+                if (row.Cells["TanggalTanam"].Value != DBNull.Value)
+                {
+                    dtpTanggalTanam.Value = Convert.ToDateTime(row.Cells["TanggalTanam"].Value);
+                }
+
+                if (row.Cells["EstimasiPanen"].Value != DBNull.Value)
+                {
+                    DateTime est = Convert.ToDateTime(row.Cells["EstimasiPanen"].Value);
+                    txtEstimasiPanen.Text = est.ToString("yyyy-MM-dd");
+                }
+            }
+        }
     }
 }
