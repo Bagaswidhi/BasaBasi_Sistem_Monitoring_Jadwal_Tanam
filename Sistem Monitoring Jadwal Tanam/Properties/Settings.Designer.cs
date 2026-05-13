dd7c92e92a0a4e0ef33e52e955f8eb3daf84b309
bagaswidhi <bagaswidhi10@gmail.com>
2026-05-13 19:26:23 +0700

    Menambahkan Data Binding pada table DataLahan

--- Sistem Monitoring Jadwal Tanam/Properties/Settings.Designer.cs
+++ Sistem Monitoring Jadwal Tanam/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Sistem_Monitoring_Jadwal_Tanam.Properties
-{
-
-
+namespace Sistem_Monitoring_Jadwal_Tanam.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MSI\\BAGAS;Initial Catalog=DBSistemMonitoringMasaTanam;User ID=sa;Pass" +
+            "word=bagas3005;TrustServerCertificate=True")]
+        public string DBSistemMonitoringMasaTanamConnectionString {
+            get {
+                return ((string)(this["DBSistemMonitoringMasaTanamConnectionString"]));
             }
         }
     }
