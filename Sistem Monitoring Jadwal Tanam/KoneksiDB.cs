c7c72baf9c4e8f7d93af6d441362ec683987800c
bagaswidhi <bagaswidhi10@gmail.com>
2026-06-25 18:35:07 +0700

    Mengambil Local IP Address sebagai koneksi database

--- Sistem Monitoring Jadwal Tanam/KoneksiDB.cs
+++ Sistem Monitoring Jadwal Tanam/KoneksiDB.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Services.Description;
+using System.Windows.Forms;
 
 namespace Sistem_Monitoring_Jadwal_Tanam
 {
@@ -12,7 +14,29 @@ namespace Sistem_Monitoring_Jadwal_Tanam
 
         public static string GetConnectionString()
         {
+            string connectionString = $"Data Source={GetLocalIpAddress()};Initial Catalog=DBSistemMonitoringMasaTanam;User ID =sa; Password=bagas3005;";
             return connectionString;
+        }
+        public static string GetLocalIpAddress()
+        {
+            string localIP = string.Empty;
+            try
+            {
+                var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+                foreach (var ip in host.AddressList)
+                {
+                    if(ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        localIP = ip.ToString();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error getting local IP address: " + ex.Message);
+            }
+            return localIP;
         }
     }
 }
